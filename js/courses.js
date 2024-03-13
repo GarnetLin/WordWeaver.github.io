fe27fa481e136c964cb1092bba06fcd0ba2e7496
Dmytro Naumchuk <just.misterr@gmail.com>
2024-03-13 21:08:31 +0100

    Courses: teachers

--- js/courses.js
+++ js/courses.js
@@ -3,7 +3,8 @@ window.addEventListener("DOMContentLoaded", () => {
 	// Option language
 
 	const lang = document.getElementById("languages"),
-		languageContent = document.querySelector(".languageContent");
+		languageContent = document.querySelector(".languageContent"),
+		ourTutors = document.querySelector(".ourTutors");
 
 
 	class OfferCard {
@@ -35,19 +36,64 @@ window.addEventListener("DOMContentLoaded", () => {
 
 			element.innerHTML = `
 			<div class='fade'>
-			<img src=${this.src} alt=${this.alt}>
-			<h3 class="offer__item-title">${this.title}</h3>
-			<div class="offer__item-descr">${this.descr}</div>
-			<div class="offer__item-divider"></div>
-			<div class="offer_item-duration">
-				<div class="offer__item-dur">Duration in hours: </div>
-				<div class="offer__item-hour"><span>${this.duration}</span> hours</div>
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="offer__item-title">${this.title}</h3>
+				<div class="offer__item-descr">${this.descr}</div>
+				<div class="offer__item-divider"></div>
+				<div class="offer__item-duration">
+					<div class="offer__item-dur">Duration in hours: </div>
+					<div class="offer__item-hour"><span>${this.duration}</span> hours</div>
+				</div>
+				<div class="offer__item-price">
+					<div class="offer__item-cost">Price per lesson:</div>
+					<div class="offer__item-total"><span>${this.price}</span> eur/lesson</div>
+				</div>
 			</div>
-			<div class="offer__item-price">
-				<div class="offer__item-cost">Price per lesson:</div>
-				<div class="offer__item-total"><span>${this.price}</span> eur/lesson</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+	class Tutors {
+		constructor(src, alt, name, experience, langs, birthplace, rate, education, description, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.name = name,
+			this.experience = experience;
+			this.education = education;
+			this.rate = rate;
+			this.langs = langs;
+			this.birthplace = birthplace;
+			this.description = description;
+			this.parent = document.querySelector(parentSelector);
+			this.classes = classes;
+		}
+
+		render() {
+			const element = document.createElement("div");
+			if (this.classes.length === 0) {
+				this.element = "tutors__pers";
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(className => element.classList.add(className));
+			}
+
+			element.innerHTML = `
+			<div class='teach'>
+				<div class='tutors__pers-left-block'>
+					<img src=${this.src} alt=${this.alt}>
+					<h3 class="tutors__pers-name">${this.name}</h3>
+					<div class='tutors__pers-birth'>${this.birthplace}</div>
+					<div class='tutors__pers-langs'>Languages: <span>${this.langs}</span></div>
+					<div class='tutors__pers-rate'>Rating: <span>${this.rate}</span></div>
+				</div>
+				<div class='tutors__pers-right-block'>
+					<div class="tutors__pers-experience">Experience in years: <span>${this.experience}</span></div>
+					<div class="tutors__pers-education">Place of education: <span>${this.education}</span></div>
+					<div class="tutors__pers-description">${this.description}</div>
+				</div>
 			</div>
-			</div>
+			<div class='divide'></div>
 			`;
 			this.parent.append(element);
 		}
@@ -71,6 +117,8 @@ window.addEventListener("DOMContentLoaded", () => {
 			showGerman();
 		}
 	}
+	
+	// Language functions
 
 	function showJapanese() {
 		new OfferCard (
@@ -91,7 +139,6 @@ window.addEventListener("DOMContentLoaded", () => {
 			300,
 			31,
 			".languageContent"
-
 		).render();
 
 		new OfferCard (
@@ -102,6 +149,45 @@ window.addEventListener("DOMContentLoaded", () => {
 			400,
 			28,
 			".languageContent"
+		).render();
+
+		new Tutors (
+			"img/teachers/molly.jpg",
+			"Molly",
+			"Molly",
+			"5",
+			"Japanese, English, French",
+			"Kyoto",
+			"5",
+			"Kyoto University",
+			"Meet Molly, your Japanese language guide. With her passion and expertise, she makes learning Japanese engaging and fun. Join her class to master the language and explore Japanese culture with confidence.",
+			".ourTutors"
+		).render();
+
+		new Tutors (
+			"img/teachers/arisu.jpg",
+			"Arisu",
+			"Arisu",
+			"3",
+			"Japanese, English, Chinese",
+			"Osaka",
+			"5",
+			"New York University",
+			"Arisu, a passionate language enthusiast deeply immersed in Japanese culture, offers a dynamic approach to teaching. With a blend of tradition and innovation, her classes promise an enriching journey through the language and culture of Japan.",
+			".ourTutors"	
+		).render();
+
+		new Tutors (
+			"img/teachers/mira.jpg",
+			"Mira",
+			"Mira",
+			"11",
+			"Japanese, English",
+			"Tokyo",
+			"4.9",
+			"The University of Tokyo",
+			"Introducing Mira, your Japanese language guru. With her vibrant teaching style and deep knowledge, she makes mastering Japanese a breeze. Join her class for an enriching journey into the language and culture of Japan.",
+			".ourTutors"
 		).render();
 	}
 
@@ -136,6 +222,46 @@ window.addEventListener("DOMContentLoaded", () => {
 			25,
 			".languageContent"
 		).render();
+
+		
+		new Tutors (
+			"img/teachers/hanna.jpg",
+			"Hanna",
+			"Hanna",
+			"9",
+			"German, English, Dutch",
+			"Hannover",
+			"5",
+			"Ludwig Maximilians University",
+			"A dedicated tutor with years of experience, Frieda specializes in making German learning accessible and enjoyable. Her patient demeanor and clear explanations ensure that students of all levels feel confident and motivated to progress in their language skills.",
+			".ourTutors"
+		).render();
+
+		new Tutors (
+			"img/teachers/frieda.jpg",
+			"Frieda",
+			"Frieda",
+			"4",
+			"German, English, Greek",
+			"München",
+			"4.9",
+			"Oxford university",
+			"Hailing from Munich, Frieda combines his love for teaching with a deep understanding of German linguistics. Whether you're a beginner or advanced learner, her tailored lessons cater to individual needs, fostering a supportive environment for language mastery.",
+			".ourTutors"	
+		).render();
+
+		new Tutors (
+			"img/teachers/emma.jpg",
+			"Emma",
+			"Emma",
+			"7",
+			"German, English, Polish",
+			"Berlin",
+			"5",
+			"The University of Mannheim",
+			"With a passion for language and a knack for effective teaching methods, Hanna brings a dynamic approach to learning German. Born and raised in Berlin, she offers insights into both language and culture, making every lesson engaging and immersive.",
+			".ourTutors"
+		).render();
 	}
 
 	function showFrench() {
@@ -168,6 +294,45 @@ window.addEventListener("DOMContentLoaded", () => {
 			300,
 			25,
 			".languageContent"
+		).render();
+
+		new Tutors (
+			"img/teachers/adeline.jpg",
+			"Adelina",
+			"Adeline",
+			"5",
+			"French, English, Italian",
+			"Paris",
+			"5",
+			"Université Paris-Saclay ",
+			"A native of Paris with a love for language and teaching, Adeline brings her expertise in French linguistics to every lesson. Her personalized approach focuses on practical communication skills, helping students develop fluency while immersing themselves in the beauty of the French language.",
+			".ourTutors"
+		).render();
+
+		new Tutors (
+			"img/teachers/alan.jpg",
+			"Alan",
+			"Alan",
+			"4",
+			"French, English, ",
+			"Vancouver",
+			"4.9",
+			"The University of British Columbia",
+			"Hailing from Vancouver, Alan is a dedicated tutor known for his patient and encouraging teaching style. With a background in French literature and language acquisition, he guides students of all levels through engaging lessons that emphasize both grammar and conversational fluency.",
+			".ourTutors"	
+		).render();
+
+		new Tutors (
+			"img/teachers/claire.jpg",
+			"Claire",
+			"Claire",
+			"3",
+			"French, English, Ukrainian",
+			"Nice",
+			"5",
+			"Sorbonne University",
+			"Passionate about sharing her love for French culture and language, Claire creates dynamic and interactive learning experiences for her students. Originally from Nice, she combines her expertise in language teaching with a deep understanding of French customs, ensuring a well-rounded education that goes beyond vocabulary and grammar.",
+			".ourTutors"
 		).render();
 	}
 
@@ -202,6 +367,45 @@ window.addEventListener("DOMContentLoaded", () => {
 			25,
 			".languageContent"
 		).render();
+		
+		new Tutors (
+			"img/teachers/lana.jpeg",
+			"Lana",
+			"Elizabeth",
+			"15",
+			"Spanish, English, French",
+			"Lake Placid",
+			"5",
+			"Fordham University in the Bronx",
+			"With a vibrant personality and a passion for Spanish culture, Lana brings energy and enthusiasm to her teaching. Born in Lake Placid, she's been always craving for Spanish culture. She offers authentic language experiences intertwined with cultural insights, making each lesson an exciting journey of discovery.",
+			".ourTutors"
+		).render();
+
+		new Tutors (
+			"img/teachers/alejandro.jpg",
+			"Alejandro",
+			"Alejandro",
+			"4",
+			"Spanish, Portuguese, English",
+			"Barcelona",
+			"4.9",
+			"Universidad Autónoma de Madrid",
+			"A seasoned tutor with a talent for simplifying complex concepts, Alejandro is known for his patient and approachable teaching style. Originally from Barcelona, he empowers his students to build confidence in their Spanish skills through interactive exercises and personalized feedback.",
+			".ourTutors"	
+		).render();
+
+		new Tutors (
+			"img/teachers/isabella.jpg",
+			"Isabella",
+			"Isabella",
+			"2",
+			"Spanish, English",
+			"Madrid",
+			"5",
+			"Autonomous University of Barcelona",
+			"As a native Spanish speaker with a background in language education, Isabelle excels in creating engaging and effective learning experiences. Her warmth and encouragement create a supportive environment where students feel motivated to explore the beauty of the Spanish language and its rich heritage.",
+			".ourTutors"
+		).render();
 	}
 
 	showGerman();
@@ -209,6 +413,10 @@ window.addEventListener("DOMContentLoaded", () => {
 	function clearContent() {
 		while (languageContent.firstChild) {
 			languageContent.removeChild(languageContent.firstChild);
+		}
+
+		while (ourTutors.firstChild) {
+			ourTutors.removeChild(ourTutors.firstChild);
 		}
 	}
 
